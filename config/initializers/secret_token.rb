d14675de7700aad08dfd366747fbfe88e0af2fab
Tatyana Kudiyarova <tatyana.kudiyarova@gmail.com>
2013-04-24 13:41:35 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatepickerDemo::Application.config.secret_token = '78661c6f087092fd2dc501aa8a063592ada264c30a61672b3d563c22bbde4826919fd7ed4a4e74284e69f8a9540ec6363afaa350318ee390cb84a065a0a143d5'
